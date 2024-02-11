6677238b3bc4d766db9f37417c958eaab238db6b
Nick <darkforceyoda@gmail.com>
2024-02-11 09:59:31 -0600

    api data

--- app/series/[seriesId]/volume/[volumeId]/page.tsx
+++ app/series/[seriesId]/volume/[volumeId]/page.tsx
@@ -5,17 +5,35 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
+import axios from "axios";
 import { Book, CheckCircle2 } from "lucide-react";
+import Image from "next/image";
 
-export default function MangaPage({ params }: { params: { mangaId: string } }) {
+export default async function MangaPage({
+  params,
+}: {
+  params: { seriesId: string; mangaId: string };
+}) {
+  const fetchedVolume = await axios.get(
+    `https://api.jikan.moe/v4/manga/${params.seriesId}/full`
+  );
+  const volume = fetchedVolume.data.data;
+
   return (
     <main>
       <div className="w-full px-4 py-2 text-center">
-        <h1 className="text-3xl font-bold">Title</h1>
-        <p className="text-gray-500">Author</p>
+        <h1 className="text-3xl font-bold"> {volume?.title_english}</h1>
+        <p className="text-gray-500">{volume.authors[0].name}</p>
       </div>
       <div className="flex justify-center items-center">
-        <div className="bg-gray-900 h-[480px] w-[80%]" />
+        <div className="h-[480px] relative w-full max-w-[420px]">
+          <Image
+            sizes="50"
+            alt="cover"
+            fill
+            src={volume.images.jpg.image_url}
+          />
+        </div>
       </div>
       <div className="w-full flex justify-center py-4 space-x-2">
         <Button size="lg">
@@ -30,12 +48,7 @@ export default function MangaPage({ params }: { params: { mangaId: string } }) {
         <div className="px-4 space-y-8">
           <h2 className="text-3xl font-semibold">Desription</h2>
           <div className="bg-gray-900 rounded px-2 py-4">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
-              sunt accusantium id tenetur beatae blanditiis dolorem commodi eius
-              voluptas nam optio itaque quas non, iste magni minima rem laborum
-              autem.
-            </p>
+            <p>{volume.synopsis}</p>
           </div>
         </div>
       </div>
